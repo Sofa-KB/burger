d29f55cc659789ed395a4705e2367f6219ff982c
SofaKB <hall.b17@gmail.com>
2020-03-11 10:19:26 -0400

    created seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Double Cheeseburger", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chili Burger", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", 0);
+
